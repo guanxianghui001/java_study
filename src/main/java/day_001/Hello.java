bd72042a68aa7a8448467ddf1a12bb4fec4bb572
guanxianghui <guanxianghui@croot.com>
2024-10-11 19:04:03 +0800

    001

--- src/main/java/day_001/Hello.java
+++ src/main/java/day_001/Hello.java
@@ -1,8 +1,35 @@
 package day_001;
-
+//面向对象-方法
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello World");
+        Person ming = new Person();
+        ming.setName("xiaoming");
+        ming.setAge(12);
+        System.out.println(ming.getName()+ming.getAge());
 
+
+    }
+}
+
+class Person{
+    private String name;
+    private int  age;
+
+    public String getName(){
+        return this.name;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setAge(int age){
+        if (age<0 || age>100)
+        {
+            throw new IllegalArgumentException("invalid name");
+        }
+        this.age = age;
     }
 }
